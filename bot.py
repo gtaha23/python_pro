853ab7643e2cfc06d7b97ccc2eaa33a038da7ef1
gtaha230812 <159329966+gtaha23@users.noreply.github.com>
2024-03-17 19:44:24 +0300

    Güncelleme bot.py
    Güncellendi 17.03.2024
    
    -GÜNCELLEMELER-
    pi ve sike komutları eklendi 🤓.
    komut sayısı 61 oldu 📢🥳.
    
    -HOŞÇAKALIN-
    Güncellemeler son hızıyla devam ediyor 😎.

--- bot.py
+++ bot.py
@@ -35,7 +35,7 @@ async def katıldı(ctx, member: discord.Member):
 
 @bot.command()
 async def yardım(ctx):
-    await ctx.send('İşte beni çağırmak için kodlar: /selam, /heh, /Gökhan, /katıldı(katıldığı tarihi öğrenmek için onun ismini yaz), /Gt_Bot, /Malike(anneme özel kod), /emoji(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,PY), /sifre(rastgele şifre oluşturur), /depo, /sağol, /milliyetçi, /yakala, /tay, /yapımcı, /mem, /mem_nadirlik, /ördek, /kedi, /köpek, /tilki, /bovcx, /santıranç, /babapiro, /bruv, /Benzema, /tarih, /ters_masa, /ben_bilmem, /csgo, /mercan, /iletişim, /git, /tek_sayilar, /masayi_duzenle, /espri, /espri_nadirlik, /bilgi, /yazılım_dili, /Discord, /don_pollo, /tl, /alman_kedy, /kurallar, /komut_sayisi, /youtube, /destek, /kodland, /client, /basic, /tester, /depoyardım, /cift_sayilar, /diğerbotlar, /uzun_kelime, /guncellemeler, /takipçi_sayisi, /RobotTom, /Bot, /GitHub ve /yardım')
+    await ctx.send('İşte beni çağırmak için kodlar: /selam, /heh, /Gökhan, /katıldı(katıldığı tarihi öğrenmek için onun ismini yaz), /Gt_Bot, /Malike(anneme özel kod), /emoji(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,PY), /sifre(rastgele şifre oluşturur), /depo, /sağol, /milliyetçi, /yakala, /tay, /yapımcı, /mem, /mem_nadirlik, /ördek, /kedi, /köpek, /tilki, /bovcx, /santıranç, /babapiro, /bruv, /Benzema, /tarih, /ters_masa, /ben_bilmem, /csgo, /mercan, /iletişim, /git, /tek_sayilar, /masayi_duzenle, /espri, /espri_nadirlik, /bilgi, /yazılım_dili, /Discord, /don_pollo, /tl, /alman_kedy, /kurallar, /komut_sayisi, /youtube, /destek, /kodland, /client, /basic, /tester, /depoyardım, /cift_sayilar, /diğerbotlar, /uzun_kelime, /guncellemeler, /takipçi_sayisi, /RobotTom, /Bot, /GitHub, /pi, /sike ve /yardım')
 
 @bot.command()
 async def tester(ctx):
@@ -136,6 +136,18 @@ async def emoji20(ctx):
 @bot.command()
 async def emojiPY(ctx):
     await ctx.send("https://cdn.discordapp.com/emojis/1139439539224580116.webp?size=128&quality=lossless")
+
+@bot.command()
+async def emojiPRO(ctx):
+    await ctx.send("https://cdn.discordapp.com/emojis/1139490575377235998.webp?size=128&quality=lossless")
+
+@bot.command()
+async def emojiCAT(ctx):
+    await ctx.send("https://cdn.discordapp.com/emojis/1215757648927129701.webp?size=128&quality=lossless")
+
+@bot.command()
+async def emojiBJK(ctx):
+    await ctx.send("https://cdn.discordapp.com/emojis/1216744301451804742.webp?size=128&quality=lossless")
 
 @bot.command()
 async def mem(ctx):
@@ -251,11 +263,15 @@ async def kurallar(ctx):
 
 @bot.command()
 async def komut_sayisi(ctx):
-    await ctx.send("Şuanda 59 komut vardır.(İlerideki hedef 60)")
+    await ctx.send("Şuanda 61 komut vardır.(İlerideki hedef 65)")
+
+@bot.command()
+async def pi(ctx):
+    await ctx.send("İlk 1000 basamağı: 3,1415926535 8979323846 2643383279 5028841971 6939937510 5820974944 5923078164 0628620899 8628034825 3421170679 8214808651 3282306647 0938446095 5058223172 5359408128 4811174502 8410270193 8521105559 6446229489 5493038196 4428810975 6659334461 2847564823 3786783165 2712019091 4564856692 3460348610 4543266482 1339360726 0249141273 7245870066 0631558817 4881520920 9628292540 9171536436 7892590360 0113305305 4882046652 1384146951 9415116094 3305727036 5759591953 0921861173 8193261179 3105118548 0744623799 6274956735 1885752724 8912279381 8301194912 9833673362 4406566430 8602139494 6395224737 1907021798 6094370277 0539217176 2931767523 8467481846 7669405132 0005681271 4526356082 7785771342 7577896091 7363717872 1468440901 2249534301 4654958537 1050792279 6892589235 4201995611 2129021960 8640344181 5981362977 4771309960 5187072113 4999999837 2978049951 0597317328 1609631859 5024459455 3469083026 4252230825 3344685035 2619311881 7101000313 7838752886 5875332083 8142061717 7669147303 5982534904 2875546873 1159562863 8823537875 9375195778 1857780532 1712268066 1300192787 6611195909 2164201989")
 
 @bot.command()
 async def guncellemeler(ctx):
-    await ctx.send("En son güncelleme: GithHub,Bot,Discord komutları eklendi 🥳 ve komut sayısı 59 oldu 📢.")
+    await ctx.send("En son güncelleme: pi ve sike komutları eklendi 🥳 ve komut sayısı 61 oldu 📢.")
 
 @bot.command()
 async def git(ctx):
@@ -312,6 +328,10 @@ async def tl(ctx):
 @bot.command()
 async def alman_kedy(ctx):
     await ctx.send("https://tenor.com/view/bingus-gunner-german-bunker-shooting-gif-20956445")
+
+@bot.command()
+async def sike(ctx):
+    await ctx.send("https://media.discordapp.net/attachments/756505124293312524/1137832647809302598/sike.gif?ex=660081bb&is=65ee0cbb&hm=e6b640725aefeec0fe02e55da066751c0c3c389fa7514e5cd6c6ae1ed7e36040&")
 
 @bot.command()
 async def kodland(ctx):
